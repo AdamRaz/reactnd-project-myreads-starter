f41c4575d76283d123186bc7cedba4e1da0c7a6e
Adam Razzak <a.abdel@hotmail.co.uk>
2018-07-29 21:16:36 +0100

    fix: book search results still display if no image data present

--- src/BookSearch.js
+++ src/BookSearch.js
@@ -51,11 +51,15 @@ class BookSearch extends React.Component {
           </div>
           <div className="search-books-results">
             <ol className="books-grid">
-              {booksArray && (booksArray.map((book) => (
+              {booksArray[0] && (booksArray.map((book) => (
                 <li key={book.id}>
                 <div className="book">
                   <div className="book-top">
-                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                    {book.imageLinks && (
+                      // a blank box will display if no imageLink info is present
+                                          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                    )}
+
                     <div className="book-shelf-changer">
                       <select>
                         <option value="move" disabled>Move to...</option>
